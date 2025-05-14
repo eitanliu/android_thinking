8cd434818b09d1434004be3f60ca1d40e2df3895
eitanliu <eitanliu@gmail.com>
2025-05-14 11:46:55 +0800

    exception handler

--- app/src/main/java/com/example/codeanalysis/util/Standard.kt
+++ app/src/main/java/com/example/codeanalysis/util/Standard.kt
@@ -1,12 +1,99 @@
 package com.example.codeanalysis.util
 
 
-inline fun catchException(crossinline block: () -> Unit): () -> Unit {
+inline fun catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: () -> Unit
+): () -> Unit {
     return {
         try {
             block()
         } catch (e: Throwable) {
-            e.printStackTrace()
+            exceptionHandler(e)
         }
     }
+}
+
+inline fun <T> catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: (T) -> Unit
+): (T) -> Unit {
+    return { t ->
+        try {
+            block(t)
+        } catch (e: Throwable) {
+            exceptionHandler(e)
+        }
+    }
+}
+
+inline fun <T1, T2> catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: (T1, T2) -> Unit
+): (T1, T2) -> Unit {
+    return { t1, t2 ->
+        try {
+            block(t1, t2)
+        } catch (e: Throwable) {
+            exceptionHandler(e)
+        }
+    }
+}
+
+inline fun <T1, T2, T3> catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: (T1, T2, T3) -> Unit
+): (T1, T2, T3) -> Unit {
+    return { t1, t2, t3 ->
+        try {
+            block(t1, t2, t3)
+        } catch (e: Throwable) {
+            exceptionHandler(e)
+        }
+    }
+}
+
+inline fun <T1, T2, T3, T4> catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: (T1, T2, T3, T4) -> Unit
+): (T1, T2, T3, T4) -> Unit {
+    return { t1, t2, t3, t4 ->
+        try {
+            block(t1, t2, t3, t4)
+        } catch (e: Throwable) {
+            exceptionHandler(e)
+        }
+    }
+}
+
+inline fun <T1, T2, T3, T4, T5> catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: (T1, T2, T3, T4, T5) -> Unit
+): (T1, T2, T3, T4, T5) -> Unit {
+    return { t1, t2, t3, t4, t5 ->
+        try {
+            block(t1, t2, t3, t4, t5)
+        } catch (e: Throwable) {
+            exceptionHandler(e)
+        }
+    }
+}
+
+inline fun <T1, T2, T3, T4, T5, T6> catchException(
+    crossinline exceptionHandler: (Throwable) -> Unit = ::exceptionPrint,
+    noinline block: (T1, T2, T3, T4, T5, T6) -> Unit
+): (T1, T2, T3, T4, T5, T6) -> Unit {
+    return { t1, t2, t3, t4, t5, t6 ->
+        try {
+            block(t1, t2, t3, t4, t5, t6)
+        } catch (e: Throwable) {
+            exceptionHandler(e)
+        }
+    }
+}
+
+
+@Suppress("NOTHING_TO_INLINE")
+inline fun exceptionPrint(e: Throwable) {
+    e.printStackTrace()
 }
